27f2f998923deaa83330e3f38570a12c3056e253
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-17 17:08:20 +0100

    improve upload functionality

--- src/js/model.js
+++ src/js/model.js
@@ -28,7 +28,7 @@ const createRecipeObject = function(data) {
 }
 export const loadRecipe = async function(id) {
   try { 
-    const data = await AJAX(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
 
     state.recipe = createRecipeObject(data);
 
@@ -46,7 +46,7 @@ export const loadSearchResults = async function(query) {
   try {
     state.search.query = query;
 
-    const data = await AJAX(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -54,6 +54,7 @@ export const loadSearchResults = async function(query) {
         title: rec.title,
         publisher: rec.publisher,
         image: rec.image_url,
+        ...(rec.key && { key: rec.key }),
       }
     })
     state.search.page = 1;
