5eacfe85677549406391c84d82e0a71b7680e0c2
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-17 14:38:58 +0100

    Bookmarked recipe list and refactoring previewView

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -19,6 +20,7 @@ const controlRecipes = async function() {
     recipeView.renderSpinner();
 
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
     // Load Recipe
     await model.loadRecipe(id);
 
@@ -63,11 +65,15 @@ const controlServings = function(newServings) {
 }
 
 const controlAddBookmark = function() {
+  // Add/ Remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
-  console.log(model.state.recipe);
+  // Update recipe view
   recipeView.update(model.state.recipe);
+
+  // render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 }
 
 const init = function() {
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,5 +1,6 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg'
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
@@ -7,30 +8,9 @@ class ResultsView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('')
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-      <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
-        <figure class="preview__fig">
-          <img src="${result.image}" alt="Test" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
-          <div class="preview__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-        </div>
-      </a>
-    </li>
-    `
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join('')
   }
 }
 
